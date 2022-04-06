5ee0145b1296f995b017d3ac58ae3d37e3f52c3b
Rastislav Pánis <rastislav.panis@gjensidige.no>
2022-04-06 14:20:49 +0200

    Fibonacci assert 3 - green

--- src/main/kotlin/xp/tdd/examples/fibonachi/Fibonacci.kt
+++ src/main/kotlin/xp/tdd/examples/fibonachi/Fibonacci.kt
@@ -3,6 +3,7 @@ package xp.tdd.examples.fibonachi
 class Fibonacci {
     fun fib(n: Int): Int {
         if (n == 0) return 0
-        return 1
+        if (n <= 2) return 1
+        return 2
     }
 }
