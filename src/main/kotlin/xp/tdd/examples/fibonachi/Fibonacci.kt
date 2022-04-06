d1b640541e06511e61e90d0a9c5732c43d8ee3f8
Rastislav Pánis <rastislav.panis@gjensidige.no>
2022-04-06 13:47:16 +0200

    Fibonacci assert 0 - green

---
+++ src/main/kotlin/xp/tdd/examples/fibonachi/Fibonacci.kt
@@ -0,0 +1,7 @@
+package xp.tdd.examples.fibonachi
+
+class Fibonacci {
+    fun fib(n: Int): Int {
+        return 0
+    }
+}
--- src/test/kotlin/xp/tdd/examples/fibonachi/FibonacciTest.kt
+++ src/test/kotlin/xp/tdd/examples/fibonachi/FibonacciTest.kt
@@ -6,7 +6,8 @@ import org.junit.jupiter.api.Test
 class FibonacciTest {
 
     @Test
-    fun `aaa` () {
-        assertThat(Fibonacci.fib(0)).isEqualTo(0)
+    fun `Fibonacci returns correct value`() {
+        val sut = Fibonacci()
+        assertThat(sut.fib(0)).isEqualTo(0)
     }
 }
